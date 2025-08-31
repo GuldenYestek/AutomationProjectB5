2b184da9b90f89b3d18f4dbc34292c3357d06966
guldenyestekova <gyestekova@gmail.com>
2025-08-31 00:06:04 -0400

    Automatiom day9

---
+++ src/test/java/io/loop/test/SELENIUM/Java/JavaJava/constractor/ParentDemo.java
@@ -0,0 +1,20 @@
+package io.loop.test.SELENIUM.Java.JavaJava.constractor;
+
+public class ParentDemo {
+
+    String name = "Rahul";
+
+    public ParentDemo() {
+        System.out.println("Parent class constructor");
+    }
+
+    public void  getData(){
+        System.out.println("Im in the parent class");
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
--- src/test/java/io/loop/test/practice/day08homework/ExtraTask.java
+++ src/test/java/io/loop/test/practice/day08homework/ExtraTask.java
@@ -1,3 +1,4 @@
+
 package io.loop.test.practice.day08homework;
 /*
  extra task
@@ -6,14 +7,47 @@ package io.loop.test.practice.day08homework;
  automate till payment
  https://app.docuport.app/company-formation/resident
  */
-
+/*
 import io.loop.test.base.TestBase;
+import io.loop.test.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class ExtraTask extends TestBase {
     @Test
-    public void extraTask() {
+    public void extraTask() throws InterruptedException {
         driver.get("https://app.docuport.app/company-formation/resident");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+        WebElement typeOfBusiness = driver.findElement(By.xpath("//*[text()='Corporation']"));
+        typeOfBusiness.click();
+
+        WebElement formationState = driver.findElement(By.xpath("//*[text()='Maryland']"));
+        formationState.click();
+
+        Thread.sleep(1000);
+
+        WebElement companyName = driver.findElement(By.xpath("//input[@id='input-103']"));
+        companyName.sendKeys("Maryland crabs");
+/*
+        Thread.sleep(1000);
+
+        WebElement DBA = driver.findElement(By.xpath("//label[@for='input-80']"));
+        companyName.sendKeys("crabs");
+
+        WebElement industry = driver.findElement(By.xpath("//input[@data-gtm-form-interact-field-id='0']"));
+        industry.click();
+
+        WebElement industryDropDown = driver.findElement(By.xpath("//*[text()='Food Services']"));
+
+
+
+
+
+
 
     }
 }
+*/
